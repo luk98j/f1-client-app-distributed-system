f212f4cf711b7549f29ed3de03d91594f8e59c6c
Ukson <jankowskilukasz1337@gmail.com>
2022-08-31 23:21:40 +0200

    Add exe build

--- src/main/java/com/f1distributedsystem/f1clientapp/ApplicationUi.java
+++ src/main/java/com/f1distributedsystem/f1clientapp/ApplicationUi.java
@@ -2,10 +2,12 @@ package com.f1distributedsystem.f1clientapp;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -23,6 +25,13 @@ public class ApplicationUi extends Application {
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent e) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
     @Override
--- src/main/java/com/f1distributedsystem/f1clientapp/service/MonitorService.java
+++ src/main/java/com/f1distributedsystem/f1clientapp/service/MonitorService.java
@@ -3,12 +3,16 @@ package com.f1distributedsystem.f1clientapp.service;
 import com.f1distributedsystem.f1clientapp.business.packet.PacketDecoder;
 import com.f1distributedsystem.f1clientapp.business.sender.PacketSender;
 import com.f1distributedsystem.f1clientapp.business.telemetry.TelemetryMonitor;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.rgielen.fxweaver.core.FxmlView;
@@ -39,17 +43,23 @@ public class MonitorService  {
     @Autowired
     private TelemetryMonitor telemetryMonitor;
 
-
-    //    @Override
-//    public void initialize(URL location, ResourceBundle resources) {
-//
-//    }
+    private Thread thread;
 
     public void startUdpServer(ActionEvent event){
         if(portText.getText() != null && portText.getText().length() > 2 && portText.getText().matches("[0-9]+")){
             errorField.setText("");
             String uniqueID = UUID.randomUUID().toString();
             userKey.setText(uniqueID);
+            thread = runServer(uniqueID);
+        } else {
+            errorField.setText("Something went wrong with port number");
+        }
+    }
+
+
+
+    private Thread runServer(String uniqueID){
+       Thread thread = new Thread( ()->{
             try{
                 telemetryMonitor.setUniqueId(uniqueID);
                 telemetryMonitor.setPort(Integer.parseInt(portText.getText()));
@@ -59,8 +69,9 @@ public class MonitorService  {
                 errorField.setText("Something went wrong with client");
                 log.error(e.getMessage());
             }
-        } else {
-            errorField.setText("Something went wrong with port number");
-        }
+        });
+       thread.start();
+       return thread;
     }
+
 }
